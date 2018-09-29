b1ecc46dbdde2aaf92ae17a200aabfce99b74cd8
Ale <a.patafi@laim-e.it>
2018-09-29 15:58:39 +0200

    somma e media

--- esercizi-stringhe-numeri/somma-e-media.js
+++ esercizi-stringhe-numeri/somma-e-media.js
@@ -3,3 +3,16 @@
   Scrivi un programma che prenda in input 5 numeri a tua scelta e restituisca in output la somma e la media.
   http://www.imparareaprogrammare.it
 */
+
+
+var a = 7;
+var b = 5;
+var c = 8;
+var d = 6;
+var e = 9;
+
+
+var sommaVoti = Math.round (a + b + c + d + e);
+var mediaVoti = Math.round (sommaVoti/5);
+
+console.log(`La somma dei voti delle cinque materie e di ${sommaVoti}, la mia media e del ${mediaVoti}`);
