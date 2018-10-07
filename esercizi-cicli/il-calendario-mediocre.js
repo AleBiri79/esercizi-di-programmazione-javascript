34e176282dcb2f70693ec56e557e4c49857f652d
Ale <a.patafi@laim-e.it>
2018-10-07 18:09:30 +0200

    il-calendario-mediocre.js

--- esercizi-cicli/il-calendario-mediocre.js
+++ esercizi-cicli/il-calendario-mediocre.js
@@ -1,4 +1,4 @@
-/*
+    /*
   Il calendario mediocre
   Scrivi un programma che stampi il calendario di un mese, ricevi in input:
     - Il numero di giorni nel mese
@@ -8,3 +8,44 @@
   Piuttosto che passarein input i giorni del mese passa direttamente il mese e calcola tu i giorni corrispondenti.
   http://www.imparareaprogrammare.it
 */
+
+var mese='Maggio';
+var numInizioSett=0;
+var giorni;
+var giorniDellaSettimana;
+switch (mese) {
+    case 'Gennaio': case 'Marzo': case 'Maggio': case 'Luglio': case 'Agosto': case 'Ottobre': case 'Dicembre': giorni=31;
+    break;
+    case 'Febbraio': giorni=28;
+    break;
+    case 'Aprile': case 'Giugno': case 'Settembre': giorni=30;
+    break;
+}
+
+for (var u=0;
+u<=giorni;
+u++) {
+    switch(numInizioSett) {
+        case 0: giorniDellaSettimana='Lunedì';
+        break;
+        case 1: giorniDellaSettimana='Martedì';
+        break;
+        case 2: giorniDellaSettimana='Mercoledì';
+        break;
+        case 3: giorniDellaSettimana='Giovedì';
+        break;
+        case 4: giorniDellaSettimana='Venerdì';
+        break;
+        case 5: giorniDellaSettimana='Sabato';
+        break;
+        case 6: giorniDellaSettimana='Domenica';
+        break;
+    }
+    console.log(giorniDellaSettimana, u);
+    if (numInizioSett < 6) {
+        numInizioSett++;
+    }
+    else {
+        numInizioSett=0;
+    }
+}
