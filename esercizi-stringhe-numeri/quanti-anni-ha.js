4bf3eb69f4e571f0d716947651ad0942996ab459
Ale <a.patafi@laim-e.it>
2018-10-09 17:56:45 +0200

    Update quanti-anni-ha.js

--- esercizi-stringhe-numeri/quanti-anni-ha.js
+++ esercizi-stringhe-numeri/quanti-anni-ha.js
@@ -3,11 +3,9 @@
   Scrivi un programma che prenda in input un anno di nascita e determini l'età della persona, calcola anche quanti anni sono necessari per raggiungere i 100. Stampa in output entrambi i risultati.
   http://www.imparareaprogrammare.it
 */
- var anno = 1979;
+var annoNascita = 1979;
 var annoAttuale = 2018;
-var centoAnni = 61;
+var anni = annoAttuale - annoNascita;
+var centoAnni = 100-anni;
 
-var totaleAnni = Math.ceil(annoAttuale - anno);
-var cento = Math.ceil(totaleAnni + centoAnni);
-
-console.log(`Quest'anno ho compiuto ${totaleAnni} anni, per arrivare a ${cento} anni mi mancano ${centoAnni} anni.`);
+console.log(`Quest'anno ho compiuto ${anni} anni, per arrivare a 100 mi mancano ${centoAnni} anni.`);
