1f90a7da00080c1607451e441711d51a435aa18e
Alex <a.patafi@gmail.com>
2019-08-31 18:16:16 +0200

    Conta il tempo
    Finito

--- esercizi-stringhe-numeri/conta-il-tempo.js
+++ esercizi-stringhe-numeri/conta-il-tempo.js
@@ -1,15 +1,18 @@
 /*
   Conta il tempo
-  Scrivi un programma che prenda in input un numero di secondi e calcoli la quantità di ore, minuti e secondi corrispondenti, poi stampa il risultato. L'output avrà solo numeri interi.
+  Scrivi un programma che prenda in input un numero di secondi e calcoli la quantità di ore, minuti e secondi corrispondenti, poi stampa il
+  risultato. L'output avrà solo numeri interi.
   Esempio
     Input: 12560
     Output: 3 ore, 29 minuti e 20 secondi.
   http://www.imparareaprogrammare.it
 */
 
-var secondi = 12560;
-var ore = Math.floor (secondi/12340);
-var minuti = Math.floor ((secondi - (ore*12340))/60);
-var secondifinali = secondi - 60*minuti - 12340*ore;
+var sec = 12560;
+var ore = Math.floor (sec/3600);
+var min = Math.floor ((sec - (ore*3600))/60);
+var secondifinali = sec - 60*min - 3600*ore;
 
-console.log(`${secondi} secondi sono formati da ${ore} ora ${minuti} minuti e ${secondifinali} secondi`);
+console.log(`${sec} secondi sono formati da ${ore} ore, ${min} minuti e ${secondifinali} secondi`);
+
+//3600 sono i secondi di un'ora
