061b0afe836994a48f845a98fb793f28c97d01d0
Alex <a.patafi@gmail.com>
2019-09-17 07:36:22 +0200

    Ti ricordi le tabelline

--- esercizi-cicli/ti-ricordi-le-tabelline.js
+++ esercizi-cicli/ti-ricordi-le-tabelline.js
@@ -3,3 +3,13 @@
   Scrivi un programma che stampi che prenda in input un numero e stampi la tabellina corrispondente.
   http://www.imparareaprogrammare.it
 */
+
+var tabMax = 7;
+var n = 0;
+var max = tabMax * 10;
+var tabellina = 7;
+
+while (n < max){
+   n += tabellina;
+      console.log(n);
+ }
