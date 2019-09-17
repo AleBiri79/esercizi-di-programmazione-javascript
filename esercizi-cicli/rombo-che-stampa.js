f7866051ca3856a4e677804530d62d88f1de871b
Alex <a.patafi@gmail.com>
2019-09-17 07:36:08 +0200

    Rombo che stampa

--- esercizi-cicli/rombo-che-stampa.js
+++ esercizi-cicli/rombo-che-stampa.js
@@ -11,3 +11,16 @@
   Hint: per indentare puoi usare il tab con '\t'.
   http://www.imparareaprogrammare.it
 */
+
+var primaRiga = ' 1';
+var secRiga = '';
+var terzRiga = '';
+
+for( a = 1; a <= 3; a++){
+  secRiga = secRiga + a;
+}
+for( a = 1; a <= 5; a++){
+  terzRiga = terzRiga + a;
+}
+
+console.log(' \t\t ' + primaRiga + '\n' + ' \t\t ' + secRiga + '\n' + '\t\t' + terzRiga + '\n' + ' \t\t ' + secRiga + '\n' + ' \t\t ' + primaRiga);
