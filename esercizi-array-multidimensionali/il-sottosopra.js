84b3c57d3496e7ad95a9c567c92bc15ef05f61b2
Alex <a.patafi@gmail.com>
2019-09-19 20:07:03 +0200

    Test

--- esercizi-array-multidimensionali/il-sottosopra.js
+++ esercizi-array-multidimensionali/il-sottosopra.js
@@ -1,7 +1,7 @@
 /*
   Il sottosopra
   Scrivi un programma che prenda in input un array multidimensionale e stampi in output la sua trasposta, in questo modo:
-    Input : 
+    Input :
       [
         [1, 2],
         [3, 4],
@@ -12,6 +12,15 @@
         [1, 3, 5],
         [2, 4, 6],
       ]
-
   http://www.imparareaprogrammare.it
 */
+
+var input = [1,2,3,4,5,6];
+var nPari = input.filter(function evenInput (numeri){
+  return numeri % 2 === 0;
+});
+var nDispari = input.filter(function evenInput (numeri){
+  return numeri % 2 === 1;
+});
+
+console.log(`Numeri pari= ${nPari} \n\nNumeri dispari= ${nDispari}`);
